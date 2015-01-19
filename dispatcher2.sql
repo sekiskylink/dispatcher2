437fe863f9e02975c7ce1b43d295741d9b591c83
Samuel Sekiwere <sekiskylink@gmail.com>
2015-01-19 16:27:21 +0300

    Add tables specific to management web interfaces

--- dispatcher2.sql
+++ dispatcher2.sql
@@ -32,3 +32,57 @@ INSERT INTO servers (name, username, password, ipaddress, url, auth_method)
     VALUES
         ('localhost', 'tester', 'foobar', '127.0.0.1', 'http://localhost:8080/test', 'Basic'),
         ('dhis2', 'tester', 'foobar', '127.0.0.1', 'http://hmis2.health.go.ug/api/dataValueSets', 'Basic');
+
+-- Add some tables for the web interface
+
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- for web.py sessions
+CREATE TABLE sessions (
+    session_id CHAR(128) UNIQUE NOT NULL,
+    atime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    data TEXT
+);
+
+CREATE TABLE user_roles (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    cdate TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_role TEXT NOT NULL UNIQUE,
+    descr text DEFAULT ''
+);
+
+CREATE TABLE user_role_permissions (
+    id bigserial NOT NULL PRIMARY KEY,
+    user_role BIGINT NOT NULL REFERENCES user_roles ON DELETE CASCADE ON UPDATE CASCADE,
+    Sys_module TEXT NOT NULL, -- the name of the module - defined above this level
+    sys_perms VARCHAR(16) NOT NULL,
+    unique(sys_module,user_role)
+);
+
+CREATE TABLE users (
+    id bigserial NOT NULL PRIMARY KEY,
+    cdate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL, -- blowfish hash of password
+    email TEXT,
+    user_role  BIGINT NOT NULL REFERENCES user_roles ON DELETE RESTRICT ON UPDATE CASCADE,
+    transaction_limit TEXT DEFAULT '0/'||to_char(NOW(),'yyyymmdd'),
+    is_active BOOLEAN NOT NULL DEFAULT 't',
+    is_system_user BOOLEAN NOT NULL DEFAULT 'f'
+
+);
+
+-- Data Follows
+INSERT INTO user_roles(user_role, descr)
+VALUES('Administrator','For the Administrators');
+
+INSERT INTO user_role_permissions(user_role, sys_module,sys_perms)
+VALUES
+        ((SELECT id FROM user_roles WHERE user_role ='Administrator'),'Users','rmad');
+
+INSERT INTO users(firstname,lastname,username,password,email,user_role,is_system_user)
+VALUES
+        ('Samuel','Sekiwere','admin',crypt('admin',gen_salt('bf')),'sekiskylink@gmail.com',
+        (SELECT id FROM user_roles WHERE user_role ='Administrator'),'t');
